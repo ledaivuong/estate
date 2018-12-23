8ac2d4025b3e62738d83cc788f81eab9a8f76ee9
Le Dai Vuong <v000770@pc-000770-3.local>
2018-12-23 23:09:10 +0700

    gh

---
+++ build/precache-manifest.df13d4943345989e0ac69ef1c4e72442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cef7d1835ceabd95a23d96385674fbe1",
+    "url": "/estate/static/media/real-estate-background-1.cef7d183.jpg"
+  },
+  {
+    "revision": "ac50fb64128b89072acf",
+    "url": "/estate/static/js/runtime~main.ac50fb64.js"
+  },
+  {
+    "revision": "4f7572184def1d13cd16",
+    "url": "/estate/static/js/main.4f757218.chunk.js"
+  },
+  {
+    "revision": "6053b82efa72d1ffde66",
+    "url": "/estate/static/js/1.6053b82e.chunk.js"
+  },
+  {
+    "revision": "4f7572184def1d13cd16",
+    "url": "/estate/static/css/main.01264b45.chunk.css"
+  },
+  {
+    "revision": "c8ee258be00e31d5ba3cbb463c24be3d",
+    "url": "/estate/index.html"
+  }
+];
